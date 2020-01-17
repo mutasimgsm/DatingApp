bc9b4f94088932fd3bb9fb41a3ff41e2fe78cc49
Mutasim GSM <mutasimgsm@yahoo.com>
2020-01-17 12:00:00 +0530

    End of Section 3

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> oprion) : base (oprion) {  }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
-}
+} 
